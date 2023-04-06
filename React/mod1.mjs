7996a8e8b33f4cd8b9364c5ad5e5de3d73186027
farneet24 <111075591+farneet24@users.noreply.github.com>
2023-04-06 13:00:35 +0530

    Add files via upload

---
+++ React/mod1.mjs
@@ -0,0 +1,8 @@
+const a = 'Farneet';
+const b = "Singh"
+
+const c = "Sadhvi"
+const d = "Sharma"
+
+export default d;
+export {a, c, b};
---
+++ React/myapp.mjs
@@ -0,0 +1,4 @@
+import surname, {a, b, c} from "./mod1.mjs"
+
+console.log(a + " " + b);
+console.log(c + " " +  surname);
